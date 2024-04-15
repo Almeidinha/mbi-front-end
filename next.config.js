2b0d658148b4fd4e57f446529e2ebad807b2426e
Marcos Almeida <blackheal@gmail.com>
2024-04-14 21:54:37 -0300

    fix: next config

--- next.config.js
+++ next.config.js
@@ -1,37 +1,19 @@
 /** @type {import('next').NextConfig} */
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn2.thedogapi.com',
-      },
-    ],
-  },
-  env: {
-    NEXTAUTH_URL: "https://1460-177-24-162-3.ngrok-free.app",
-    BASE_URL: "http://localhost:8081/api/v1",
-  },
-};
-
-
 module.exports = (phase, { defaultConfig }) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      ...nextConfig,
-      env: {
-        NEXTAUTH_URL: "http://localhost:3000",
-        BASE_URL: "http://localhost:8081/api/v1",
-      },
-    }
-  }
-
-  return nextConfig
+  return {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'lh3.googleusercontent.com',
+        },
+      ],
+    },
+    env: {
+      NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+      BASE_URL: process.env.BASE_URL,
+    },
+  };
 }
-
